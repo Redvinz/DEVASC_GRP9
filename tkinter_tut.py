a543e7523a33f7e527ad737bba8366ddee410b3e
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-10-04 13:06:41 +0800

    updated with OOP

--- tkinter_tut.py
+++ tkinter_tut.py
@@ -1,53 +1,58 @@
 import tkinter as tk
 from tkinter import ttk
 
-root = tk.Tk()
-root.title("Simple App")
+
+def main():
+    app = Application()
+    app.mainloop()
 
 
-def add_to_list(event=None):
-    text = entry.get()
-    if text:
-        text_list.insert(tk.END, text)
-        entry.delete(0, tk.END)
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Simple App")
+
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=3)
+        self.rowconfigure(0, weight=1)
+
+        frame = InputForm(self)
+        frame.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+
+        frame2 = InputForm(self)
+        frame2.grid(row=0, column=1, sticky="nsew", padx=5, pady=5)
 
 
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=3)
-root.rowconfigure(0, weight=1)
+class InputForm(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
 
-frame = ttk.Frame(root)
-frame.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+        self.entry = ttk.Entry(self)
+        self.entry.grid(row=0, column=0, sticky="ew")
 
-frame.columnconfigure(0, weight=1)
-frame.rowconfigure(1, weight=1)
+        self.entry.bind("<Return>", self.add_to_list)
 
-entry = ttk.Entry(frame)
-entry.grid(row=0, column=0, sticky="ew")
+        self.entry_btn = ttk.Button(self, text="Add", command=self.add_to_list)
+        self.entry_btn.grid(row=0, column=1)
 
-entry.bind("<Return>", add_to_list)
+        self.clear_btn = ttk.Button(self, text="Clear", command=self.clear_list)
+        self.clear_btn.grid(row=0, column=2)
 
-entry_btn = ttk.Button(frame, text="Add", command=add_to_list)
-entry_btn.grid(row=0, column=1)
+        self.text_list = tk.Listbox(self)
+        self.text_list.grid(row=1, column=0, columnspan=3, sticky="nsew")
 
-text_list = tk.Listbox(frame)
-text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
+    def add_to_list(self, event=None):
+        text = self.entry.get()
+        if text:
+            self.text_list.insert(tk.END, text)
+            self.entry.delete(0, tk.END)
 
-frame2 = tk.Frame(root)
-frame2.grid(row=0, column=1, sticky="nsew", padx=5, pady=5)
+    def clear_list(self):
+        self.text_list.delete(0, tk.END)
+        self.entry.delete(0, tk.END)
 
-frame2.columnconfigure(0, weight=1)
-frame2.rowconfigure(1, weight=1)
 
-entry = tk.Entry(frame2)
-entry.grid(row=0, column=0, sticky="ew")
-
-entry.bind("<Return>", add_to_list)
-
-entry_btn = tk.Button(frame2, text="Add", command=add_to_list)
-entry_btn.grid(row=0, column=1)
-
-text_list = tk.Listbox(frame2)
-text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
-
-root.mainloop()
+if __name__ == "__main__":
+    main()
